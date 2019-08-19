d5ed87ae0420ac1ee18d47fa2d152299698a609d
Oryxamus <greg@csprousers.org>
2019-08-19 16:41:53 -0400

    updated the Help Tools for Visual Studio 2019

--- Create Help Tools.bat
+++ Create Help Tools.bat
@@ -1,6 +1,6 @@
-set msbuild="C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe"
+set msbuild="C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe"
 if exist %msbuild% goto :start
-set msbuild="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
+set msbuild="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"
 
 :start
 %msbuild% "Help Tools\Help Tools.sln" /p:Configuration=Release /t:Build
