7b073053caf1d85d6a258ba28824e28aac175403
Orxyamus <greg@csprousers.org>
2022-01-24 18:50:13 -0500

    upgraded the Help Tools to use Visual Studio 2022

--- Create Help Tools.bat
+++ Create Help Tools.bat
@@ -1,6 +1,6 @@
-set msbuild="C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe"
+set msbuild="C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MSBuild.exe"
 if exist %msbuild% goto :start
-set msbuild="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"
+set msbuild="C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe"
 
 :start
 %msbuild% "Help Tools\Help Tools.sln" /p:Configuration=Release /t:Build
